3cce5e01c4345795fdd44f8899b8a234e116ba1f
Abdu Ssekalala <assekalala@gmail.com>
2016-02-29 09:34:52 +0300

    changed to mysql

--- getin/settings.py
+++ getin/settings.py
@@ -68,11 +68,13 @@ WSGI_APPLICATION = 'getin.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'getin',
+        'HOST': '127.0.0.1',
+        'USER': 'root',
+        'PASS': ''
     }
 }
-
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
 
